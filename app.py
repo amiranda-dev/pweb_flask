0e20195256a67a61358931e681ae2ac79b649f80
Alisson Miranda <alissonmichelmiranda@gmail.com>
2025-11-22 00:17:52 -0300

    criando a rota turma, sql e página

--- app.py
+++ app.py
@@ -20,6 +20,22 @@ def listar_aluno():
     return render_template('aluno/lista.html',lista=lista)
 
 
+@app.route('/turma')
+def listar_turma():
+    DB_PATH = "banco_escola.db"
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(''' 
+                        SELECT turma.id, semestre, nome_curso, professor.nome  FROM turma 
+                        JOIN curso on curso.id=turma.curso_id
+                        JOIN professor on professor.id=turma.professor_id
+                    '''
+                )
+    lista = cursor.fetchall()
+    conn.close()
+    return render_template('turma/lista.html',lista=lista)
+
+
 @app.route('/professor')
 def listar_professor():
     DB_PATH = "banco_escola.db"
@@ -29,6 +45,8 @@ def listar_professor():
     lista = cursor.fetchall()
     conn.close()
     return render_template('professor/lista.html',lista=lista)
+
+
 @app.route('/sobre') # uma rota (decorator)
 def sobre():
     return render_template('sobre.html')
